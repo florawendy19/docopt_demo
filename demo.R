9e26a8a3e780e369066d3224297dc5435d7f2093
florawendy19 <90071253+florawendy19@users.noreply.github.com>
2022-11-19 15:35:06 -0800

    Update demo.R

--- demo.R
+++ demo.R
@@ -3,9 +3,11 @@
 
 "This script prints out docopt args.
 Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> [<new_pos_arg>] --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
+[<new_pos_arg>]   Takes any value (this is a  positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
